9ebf96db5281a54e1e751aec2f2ab1ba9f214453
VictorVillazante <villazante.bernabe.victor@gmail.com>
2022-12-14 00:09:34 -0400

    put lazy loading in images

--- src/views/GiftsView.vue
+++ src/views/GiftsView.vue
@@ -3,7 +3,7 @@
     <div class="row">
         <div v-for="gift in gifts" class="col-12 col-md-4" :key="gift.id">
             <div class="card p-3" >
-                <img :src="gift.images.original.url" class="card-img-top" alt="...">
+                <img :src="gift.images.original.url" class="card-img-top" :alt="gift.id" loading="lazy">
                 <div class="card-body">
                     <h5 class="card-title">{{ gift.title }}</h5>
                     <a href="#" class="btn btn-primary">Download</a>
